e2a2a48a659f2c94e720cf43c299f9f4115a4642
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-03-11 20:59:58 -0300

    feat: add Category-item component

---
+++ src/components/category-item/Category-item.tsx
@@ -0,0 +1,24 @@
+import { FunctionComponent } from 'react';
+
+// Utilities
+import Category from '../../types/category.types';
+
+// Styles
+import { CategoryItemContainer, CategoryName } from './Category-item.style';
+
+interface CategoryItemProps {
+  category: Category;
+}
+
+export const CategoryItem: FunctionComponent<CategoryItemProps> = ({
+  category,
+}) => {
+  return (
+    <CategoryItemContainer backgroundImage={category.imageUrl}>
+      <CategoryName>
+        <p>{`${category.displayName}`}</p>
+        <p>Explorar</p>
+      </CategoryName>
+    </CategoryItemContainer>
+  );
+};
