b48a934f7ed5f24ec4af2e4623728c59052a92cd
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-03-11 20:11:09 -0300

    feat: add HomePage component and React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { HomePage } from './pages/home/Home.pages';
+
 export const App = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
