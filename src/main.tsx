8f4dd79a03c2ca6c37b93607ebff8aa60e5c2f08
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-03-11 19:58:58 -0300

    style: add global style and theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import { GlobalStyle } from './main.style.ts';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,16 @@
+export const Colors = {
+  primary: '#212529',
+  text: {
+    white: '#f8f9fa',
+    dark: '#343a40',
+  },
+  background: {
+    dark: '#212529',
+    white: '#f8f9fa',
+  },
+  input: {
+    background: '#e9ec3f',
+    placeholder: '#b6c757d',
+  },
+  error: '#ff6a6a',
+};
