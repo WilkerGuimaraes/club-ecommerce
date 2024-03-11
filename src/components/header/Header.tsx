2791fe739ce726c22863178301a0803d852c4481
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-03-11 20:06:35 -0300

    feat: add Header Component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,26 @@
+import { BsCart3 } from 'react-icons/bs';
+
+import {
+  HeaderContainer,
+  HeaderTitle,
+  HeaderItems,
+  HeaderItem,
+} from './Header.style';
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <HeaderTitle>CLUB CLOTHING</HeaderTitle>
+
+      <HeaderItems>
+        <HeaderItem>Explorar</HeaderItem>
+        <HeaderItem>Login</HeaderItem>
+        <HeaderItem>Criar Conta</HeaderItem>
+        <HeaderItem>
+          <BsCart3 size={25} />
+          <p style={{ marginLeft: 5 }}>5</p>
+        </HeaderItem>
+      </HeaderItems>
+    </HeaderContainer>
+  );
+};
