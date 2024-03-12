1464c4650d9bd7063bcf5be05c47a9938808183b
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-03-11 22:03:53 -0300

    feat: get categories from firestore db

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -1,10 +1,51 @@
+import { useEffect, useState } from 'react';
+import { getDocs, collection } from 'firebase/firestore';
+
+// Components
+import { CategoryItem } from '../category-item/Category-item';
+
+// Utilities
+import Category from '../../types/category.types';
+import { db } from '../../config/firebase.config';
+import { categoryConverter } from '../../converters/firestore.converters';
+
 // Styles
 import { CategoriesContainer, CategoriesContent } from './Categories.style';
 
 export const Categories = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  const fetchCategories = async () => {
+    try {
+      const categoriesFromFirestore: Category[] = [];
+
+      const querySnapshot = await getDocs(
+        collection(db, 'categories').withConverter(categoryConverter),
+      );
+
+      querySnapshot.forEach((doc) => {
+        categoriesFromFirestore.push(doc.data());
+      });
+
+      setCategories(categoriesFromFirestore);
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
   return (
     <CategoriesContainer>
-      <CategoriesContent></CategoriesContent>
+      <CategoriesContent>
+        {categories.map((category) => (
+          <div key={category.id}>
+            <CategoryItem category={category} />
+          </div>
+        ))}
+      </CategoriesContent>
     </CategoriesContainer>
   );
 };
