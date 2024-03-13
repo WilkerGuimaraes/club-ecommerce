83e17a6116423251e8b172d317562ac3916d91ee
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-03-13 14:47:06 -0300

    feat: add home navigation to logo click

--- src/components/header/Header.styles.ts
+++ src/components/header/Header.styles.ts
@@ -13,6 +13,10 @@ export const HeaderContainer = styled.div`
 export const HeaderTitle = styled.h2`
   font-weight: 700;
   font-size: 1.5rem;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export const HeaderItems = styled.div`
