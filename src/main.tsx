37c0d1d7d19450285d9c78638ff379c98daef78d
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-03-13 12:03:27 -0300

    feat: add UserContext

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
 
+import { UserContextProvider } from './contexts/user.context.tsx';
+
 import { GlobalStyle } from './main.style.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <UserContextProvider>
+      <GlobalStyle />
+      <App />
+    </UserContextProvider>
   </React.StrictMode>,
 );
