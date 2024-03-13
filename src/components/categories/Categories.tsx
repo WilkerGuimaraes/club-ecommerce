a29c396f0dbf44a2c58a10daf314bfbd778774df
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-03-13 16:31:11 -0300

    feat: add Category Context

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -1,37 +1,18 @@
-import { useEffect, useState } from 'react';
-import { getDocs, collection } from 'firebase/firestore';
+import { useContext, useEffect } from 'react';
 
 // Components
 import { CategoryItem } from '../category-item/Category-item';
+import { Loading } from '../loading/Loading';
 
 // Utilities
-import Category from '../../types/category.types';
-import { db } from '../../config/firebase.config';
-import { categoryConverter } from '../../converters/firestore.converters';
+import { CategoryContext } from '../../contexts/category.context';
 
 // Styles
 import { CategoriesContainer, CategoriesContent } from './Categories.styles';
 
 export const Categories = () => {
-  const [categories, setCategories] = useState<Category[]>([]);
-
-  const fetchCategories = async () => {
-    try {
-      const categoriesFromFirestore: Category[] = [];
-
-      const querySnapshot = await getDocs(
-        collection(db, 'categories').withConverter(categoryConverter),
-      );
-
-      querySnapshot.forEach((doc) => {
-        categoriesFromFirestore.push(doc.data());
-      });
-
-      setCategories(categoriesFromFirestore);
-    } catch (error) {
-      console.log({ error });
-    }
-  };
+  const { categories, isLoading, fetchCategories } =
+    useContext(CategoryContext);
 
   useEffect(() => {
     fetchCategories();
@@ -39,6 +20,7 @@ export const Categories = () => {
 
   return (
     <CategoriesContainer>
+      {isLoading && <Loading />}
       <CategoriesContent>
         {categories.map((category) => (
           <div key={category.id}>
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import { App } from './App.tsx';
 import { UserContextProvider } from './contexts/user.context.tsx';
 
 import { GlobalStyle } from './main.style.ts';
+import { CategoryContextProvider } from './contexts/category.context.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <UserContextProvider>
-      <GlobalStyle />
-      <App />
+      <CategoryContextProvider>
+        <GlobalStyle />
+        <App />
+      </CategoryContextProvider>
     </UserContextProvider>
   </React.StrictMode>,
 );
