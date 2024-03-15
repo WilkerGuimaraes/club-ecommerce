a399bb2dc576e4429354528539383fc6f25eb188
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-03-15 15:55:03 -0300

    feat: add stripe api integration to checkout

--- src/components/checkout/Checkout.tsx
+++ src/components/checkout/Checkout.tsx
@@ -1,5 +1,6 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { BsBagCheck } from 'react-icons/bs';
+import axios from 'axios';
 
 import { CartContext } from '../../contexts/cart.context';
 import { CustomButtom } from '../custom-buttom/Custom-buttom';
@@ -11,12 +12,35 @@ import {
   CheckoutTitle,
   CheckoutTotal,
 } from './Checkout.style';
+import { Loading } from '../loading/Loading';
 
 export const Checkout = () => {
   const { products, productsTotalPrice } = useContext(CartContext);
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleFinishPurchaseClick = async () => {
+    try {
+      setIsLoading(true);
+
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_API_URL}/create-checkout-session`,
+        {
+          products,
+        },
+      );
+
+      window.location.href = data.url;
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <CheckoutContainer>
+      {isLoading && <Loading />}
       <CheckoutTitle>Checkout</CheckoutTitle>
 
       {products.length > 0 ? (
@@ -28,7 +52,10 @@ export const Checkout = () => {
           </CheckoutProducts>
 
           <CheckoutTotal>Total: R${productsTotalPrice}</CheckoutTotal>
-          <CustomButtom startIcon={<BsBagCheck />}>
+          <CustomButtom
+            startIcon={<BsBagCheck />}
+            onClick={handleFinishPurchaseClick}
+          >
             Finalizar Compra
           </CustomButtom>
         </>
